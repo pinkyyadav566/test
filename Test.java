bccb87c1dc20e0bc21b3dde5fe773101dc414a2a
Pinky <pinkyyadav566@gmail.com>
2019-01-18 11:13:13 +0530

    Sop added

--- Test.java
+++ Test.java
@@ -3,6 +3,10 @@ class Test{
 public static void main(String args[]){
 
 System.out.println("Welcome to first java program");
+System.out.println("Welcome to second java program");
+System.out.println("Welcome to third java program");
+System.out.println("Welcome to fifth java program");
+
 
 }
 }
